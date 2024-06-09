4a6b3b51c4a4859c6de65511a443649767e39d6d
Ireme-Nyawe <ishimwevalens@gmail.com>
2024-06-09 16:43:04 +0200

    create stringtransform and html form for testing

---
+++ Challenges/stringTransform.js
@@ -0,0 +1,26 @@
+function transformString(str) {
+  const length = str.length;
+  if (length % 15 === 0) {
+    str = str.split("").reverse().join("");
+    str = str
+      .split("")
+      .map((char) => char.charCodeAt(0))
+      .join("");
+  } else if (length % 3 === 0) {
+    str = str.split("").reverse().join("");
+  } else if (length % 5 === 0) {
+    str = str
+      .split("")
+      .map((char) => char.charCodeAt(0))
+      .join("");
+  }
+  return str;
+}
+document
+  .getElementById("stringForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const stringInput = document.getElementById("stringInput").value;
+    const result = transformString(stringInput);
+    document.getElementById("transformResult").innerText = result;
+  });
